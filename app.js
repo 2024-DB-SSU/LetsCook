e8aa00d0e2eb7568a43759ed9f8018575be91f33
peridot0810 <163098646+peridot0810@users.noreply.github.com>
2024-11-17 03:05:54 +0900

    회원가입/로그인 - 세션 저장까지

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
 const express = require('express');
-const app = express(); 
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require('passport')
+const LocalStrategy = require('passport-local');
+const { Sequelize } = require('sequelize');
+const MySQLStore = require('express-mysql-session')(session);
 const User = require('./class/User');
 const Fridge = require('./class/Fridge'); 
 const Server = require('./class/Server'); 
+const app = express(); 
+const server = new Server()
 
 app.use(express.static(__dirname + '/public'))
-app.set('view engine', 'ejs') // 템플릿 엔진으로 ejs 설정
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'ejs') // 템플릿 엔진으로 ejs 설정
+
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: '2024_SSU_DB', // 원하는 키로 변경
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore({
+      host: 'dbproject.cp6ikkuwamgv.ap-southeast-2.rds.amazonaws.com',
+      port: 3306,
+      user: 'admin',
+      password: 'ssumath2024',
+      database: 'LetsCook',
+      expiration: 30000,
+      createDatabaseTable: true
+    }),
+    cookie : { maxAge : 60 * 60 * 1000 }
+  })
+);
+app.use(passport.session());
+passport.use(new LocalStrategy({
+  usernameField: 'ID',
+  passwordField: 'PWD'
+}, async (ID, PWD, cb) => {
+  let result = await server.get_user(ID);
+  let user = result.user_info[0];
+  if (!user) {
+    return cb(null, false, { message: '없는 아이디 입니다' })
+  }
+  if (await bcrypt.compare(PWD, user.PWD)) {
+    return cb(null, user) 
+  } else {
+    return cb(null, false, { message: '틀린 비밀번호 입니다' });
+  }
+}))
+
+passport.serializeUser((user, done) => {
+  process.nextTick(() => { 
+    done(null, { ID: user.ID, Email: user.Email })
+  })
+})
+
+passport.deserializeUser(async (user, done) => {
+  let result = await server.get_user(user.ID);
+  user = result.user_info[0];
+  process.nextTick(() => {
+    delete user.PWD        
+    return done(null, user) 
+  })
+})
+
+
+
 
 app.listen(8080, () => {
-  server = new Server()
   console.log('http://localhost:8080 에서 실행 중입니다.');
 });
 // ======================================================================
@@ -19,24 +78,25 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
 });
 
-app.get('/test', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT * FROM recipe');  // 쿼리 실행
-    res.json(rows);  // 결과를 JSON 형식으로 응답
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Database query failed' });
-  }
-});
-
 
 app.post('/sign_in', async (req, res) => {
-  console.log(req.body.username)
-  console.log(req.body.password)
-  console.log(req.body.email)
-  const result = server.signin(req.body.username, req.body.password, req.body.email)
-  res.redirect("/main")
+  console.log(req.body);
+  let hashed = await bcrypt.hash(req.body.PWD, 10) 
+  const result = server.signin(req.body.ID, hashed, req.body.email);
+  res.redirect("/");
 });
+
+app.post('/log_in', async (req, res, next) => { 
+  passport.authenticate('local', (error, user,info)=>{
+    if (error) return res.status(500).json(error)  
+    if (!user) return res.status(401).json(info.message)
+    req.logIn(user, (err)=>{
+      if (err) return next(err)
+      res.redirect('/main')    
+    })
+  })(req, res, next)
+})
+
 
 app.get('/main', (req, res) => {
   res.render('main.ejs')
--- class/Server.js
+++ class/Server.js
@@ -6,7 +6,7 @@ class Server {
     
   }
 
-  async signin(ID, PWD, Email) {
+  async signin(ID, PWD, Email){
     try {
       const query = 'INSERT INTO User (ID, PWD, Email) VALUES (?, ?, ?)';
       const values = [ID, PWD, Email];
@@ -15,6 +15,17 @@ class Server {
     } catch (err) {
       console.error(err);
       return {status : 500, error : 'Database query failed'}
+    }
+  }
+
+  async get_user(user_ID){
+    try {
+      const query = 'SELECT * FROM User WHERE ID = ?';
+      const user_info = await db.execute(query, [user_ID]);
+      return {status : 200, error : 'No Error', user_info : user_info[0]}
+    } catch (err) {
+      console.error(err);
+      return {status : 500, error : 'Database query failed', user_info : NaN}
     }
   }
 }
